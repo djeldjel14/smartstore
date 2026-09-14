118dd8b42546c8873abb0ce7efbdb5076adf9bca
djeldjel14 <khaleddjeldjel3@gmail.com>
2026-09-14 19:26:09 +0100

    Add Admin REST API controller for managing products, categories, orders, and users

---
+++ src/main/java/com/smartstore/smartstore/dto/UserDTO.java
@@ -0,0 +1,31 @@
+package com.smartstore.smartstore.dto;
+
+import com.smartstore.smartstore.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private Boolean enabled;
+    private LocalDateTime createdAt;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.enabled = user.getEnabled();
+        this.createdAt = user.getCreatedAt();
+    }
+}
